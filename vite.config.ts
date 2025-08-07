94de6fc6e7b764aa67a387a44c4052465ee1a218
Tabuut <fgrtey88@gmail.com>
2025-08-07 19:03:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import tsconfigPaths from 'vite-tsconfig-paths' // 1. استيراد الحزمة
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths() // 2. إضافة الإضافة هنا
-  ],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      // هذا هو نفس الإعداد الموجود في tsconfig.json
-      // وهو يضمن التوافق الكامل
       "@": path.resolve(__dirname, "./src"),
     },
   },
